5a9ec70a8ff81ae447d9f53b860883592943daa1
sebriede6 <sebriede6@gmail.com>
2024-10-01 13:30:14 +0200

    "Aufgabe 1"

---
+++ # definieren der variablen Hausaufgabe 1.10.24 Python.py
@@ -0,0 +1,11 @@
+# definieren der variablen
+first_name = "Sebastian" # string
+last_name ="Riede-Zwaetz" # String
+birth_year = 1988 # ganzzahl
+has_license = True # boolean
+
+# Ausgabe der Variablen
+print("Vorname:", first_name)
+print("Nachname:", last_name)
+print("Geburtsjahr:", birth_year)
+print("hat einen Führerschein:", has_license)
---
+++ python-tutorial/python-introduction/README.md
@@ -0,0 +1,9 @@
+# definieren der variablen
+name = "sebastian Riede-Zwätz" # string
+age = 36 # ganzzahl
+height = 1.80 # fließkommazahl
+is_student = True # boolean (True or False)
+
+# ausgabe der variablen
+print("name:", name)
+print(alter) 
