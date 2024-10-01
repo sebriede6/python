37efb1939c02ffd24b56b3bcbc6262bb8f7b42e9
sebriede6 <sebriede6@gmail.com>
2024-10-01 13:47:50 +0200

    Hausaufgabe Teil 2

---
+++ Hausaufgabe teil 2.py
@@ -0,0 +1,11 @@
+# definieren der Variablen
+a = 10
+b = 3
+
+# Berechnung und Ausgabe
+print("Summe:", a + b)
+print("Differenz:", a - b)
+print("Produkt:", a * b)
+print("Ganzzahliger Quotient:", a // b)
+print("Rest der Division:", a % b)
+print("a hoch b:", a ** b)
